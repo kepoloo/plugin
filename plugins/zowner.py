463242e4c9bc1325b805cf573e12c0dc9631ae1f
Haiki <arlozayden2@gmail.com>
2024-08-06 19:58:37 +0700

    Update zowner.py

--- plugins/zowner.py
+++ plugins/zowner.py
@@ -8,39 +8,6 @@ from VIPMUSIC.mongo.afkdb import LOGGERS as OWNERS
 from VIPMUSIC.utils.database import add_served_chat, get_assistant
 
 
-@app.on_message(filters.command("repo"))
-async def help(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://te.legra.ph/file/4b52da6d880cbb199298a.jpg",
-        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
-                    )
-                ]
-            ]
-        ),
-    )
-
-
-@app.on_message(filters.command("clone"))
-async def clones(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://te.legra.ph/file/4b52da6d880cbb199298a.jpg",
-        caption=f"""**🙂You Are Not Sudo User So You Are Not Allowed To Clone Me.**\n**😌Click Given Below Button And Host Manually Otherwise Contact Owner Or Sudo Users For Clone.**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
-                    )
-                ]
-            ]
-        ),
-    )
-
 
 # --------------------------------------------------------------------------------- #
 
