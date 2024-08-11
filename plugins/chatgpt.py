5afc72e85141ab872e5528c974bee4938c99a837
Haiki <arlozayden2@gmail.com>
2024-08-12 01:58:51 +0700

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -10,38 +10,30 @@ from VIPMUSIC import app
         ["chatgpt", "ai", "ask"], prefixes=["/"]
     )
 )
-async def _(client, message):
-    prs = await EMO.PROSES(client)
-    sks = await EMO.BERHASIL(client)
-    ggl = await EMO.GAGAL(client)
+async def chatgpt_chat(bot, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        await message.reply_text(
+            "Example:\n\n`/ai write simple website code using html css, js?`"
+        )
+        return
+
+    if message.reply_to_message and message.reply_to_message.text:
+        user_input = message.reply_to_message.text
+    else:
+        user_input = " ".join(message.command[1:])
+
     try:
-        await client.send_chat_action(message.chat.id, ChatAction.TYPING)
-
-        if len(message.command) < 2:
-            await message.reply_text(
-                f"{ggl}<b>ᴍᴏʜᴏɴ ɢᴜɴᴀᴋᴀɴ ғᴏʀᴍᴀᴛ\nᴄᴏɴᴛᴏʜ : </b><code>ask bagaimana membuat donat?</code>"
-            )
-        else:
-            prs = await message.reply_text(f"<b>{prs}ᴘʀᴏᴄᴄᴇsɪɴɢ....</b>")
-            a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://chatgpt.apinepdev.workers.dev/?question={a}')
-
-            try:
-                if "answer" in response.json():
-                    x = response.json()["answer"]                  
-                    await prs.edit(
-                      f"{x}<b>{sks}ᴘᴇʀᴛᴀɴʏᴀᴀɴ ɪɴɪ ᴅɪᴊᴀᴡᴀʙ ᴏʟᴇʜ</b> : <code>{bot.me.mention}</code>"
-                    )
-                else:
-                    await message.reply_text(f"<b>{ggl}ɴᴏ 'ʀᴇsᴜʟᴛs' ᴋᴇʏ ғᴏᴜɴᴅ ɪɴ ᴛʜᴇ ʀᴇsᴘᴏɴsᴇ.</b>")
-            except KeyError:
-                await message.reply_text(f"<b>ᴇʀʀᴏʀ ᴀᴄᴄᴇssɪɴɢ ᴛʜᴇ ʀᴇsᴘᴏɴsᴇ.</b>")
-    except Exception as e:
-        await message.reply_text(f"{e}")
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        results = api.chatgpt(user_input)
+        if results["success"]:
+            await message.reply_text(results["results"])
+    except requests.exceptions.RequestException as e:
+        pass
 
 
 __MODULE__ = "ᴄʜᴀᴛɢᴘᴛ"
 __HELP__ = """
-/ai [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ᴄʜᴀᴛɢᴘᴛ's ᴀɪ
-/gemini [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ɢᴏᴏɢʟᴇ's ɢᴇᴍɪɴɪ ᴀɪ
-/bard [ǫᴜᴇʀʏ] -ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ɢᴏᴏɢʟᴇ's ʙᴀʀᴅ ᴀɪ"""
+
+/ai [query] - ask your question with chatgpt's ai
+/gemini [query] - ask your question with google's gemini ai
+/bard [query] -ask your question with google's bard ai."""
